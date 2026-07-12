4b8228b99786b81fcdd679cc0666e0ef5a468ad7
MoatazElMozien <moataz.120230264@ejust.edu.eg>
2026-07-12 06:44:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'loom-log-v1';
+const CACHE = 'loom-log-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
